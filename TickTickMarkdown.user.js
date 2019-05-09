8ea138383e6d991ff5e9d4caf25433dae7d4a1c5
leda <li_cheng_zhe@163.com>
2019-05-09 18:01:12 +0800

    match        https://ticktick.com

--- TickTickMarkdown.user.js
+++ TickTickMarkdown.user.js
@@ -4,7 +4,7 @@
 // @version      0.5
 // @description  Add markdown support to TickTick
 // @author       szhshp
-// @match        https://dida365.com/*
+// @match        https://ticktick.com/*
 // @grant        GM_addStyle
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/showdown/1.8.6/showdown.min.js
